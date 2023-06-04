9439d420e180d773fe405f4d75d0fe64fa36aad2
Ben Hadin Maryem <119045781+maryembe@users.noreply.github.com>
2023-06-04 01:42:28 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -8,6 +8,7 @@
 	<title> role-based-login-system</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<link rel="stylesheet" href="header.css" />
+	<link rel="stylesheet" href="controls.css" />
 	<style>
 		body {
 			background-image: url(3.jpg);
